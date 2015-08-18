184044dfd85f2045002ffbb49a74b3060864db0b
monarajhans <mahi.rajhans@gmail.com>
2015-08-18 15:03:19 -0700

    Client controllers

---
+++ client/client_controllers.js
@@ -0,0 +1,32 @@
+
+ myApp.controller('UsersController', function($scope, UsersFactory) {
+	        UsersFactory.getPopularProjects(function(data) {
+	        $scope.users = data;
+	        })
+
+	        UsersFactory.getRecentlyFunded(function(data) {
+	        $scope.users = data;
+	        })
+    	});
+
+        myApp.controller('CompaniesController', function($scope, $routeParams, UsersFactory) {
+        	
+        	var id = $routeParams.id;
+	        UsersFactory.getUsers(function(data) {
+	        $scope.users = data;
+	        })
+
+	        UsersFactory.get_one_company(id, function(data) {
+	        $scope.companies = data;
+	        })
+    	});	
+
+        myApp.controller('ProjectsController', function($scope, UsersFactory) {
+
+			UsersFactory.create($scope.newCompany, function() {
+					            	UsersFactory.getCompanies(function(data) {
+					            		$scope.companies = data;
+					           		 });
+					          	});
+			$scope.newCompany = {};
+    	});	
