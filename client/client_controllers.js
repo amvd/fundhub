f407d152d2a2b0d58115cf6e0924225d51144417
Armand <adeasis10@gmail.com>
2015-08-19 12:25:04 -0700

    misc formatting fixes

--- client/client_controllers.js
+++ client/client_controllers.js
@@ -1,32 +1,38 @@
- myApp.controller('UsersController', function($scope, UsersFactory) {
+myApp.controller('UsersController', function($scope, UsersFactory) {
 	        UsersFactory.getPopularProjects(function(data) {
 
 	        $scope.popular = data;
-	        })
+	        });
 
 	        UsersFactory.getRecentlyFunded(function(output) {
 	        $scope.recent_funded = output;
-	        })
+	        });
     	});
 
-        myApp.controller('CompaniesController', function($scope, $routeParams, UsersFactory) {
-        	
-        	var id = $routeParams.id;
-	        UsersFactory.getUsers(function(data) {
-	        $scope.users = data;
-	        });
+myApp.controller('CompaniesController', function($scope, $routeParams, UsersFactory) {
+	
+	var id = $routeParams.id;
+    UsersFactory.getUsers(function(data) {
+    $scope.users = data;
+    });
 
-	        UsersFactory.get_one_company(id, function(data) {
-	        $scope.companies = data;
-	        });
-    	});	
+    UsersFactory.get_one_company(id, function(data) {
+    $scope.companies = data;
+    });
+});	
 
-        myApp.controller('ProjectsController', function($scope, UsersFactory) {
+myApp.controller('ProjectsController', function($scope, UsersFactory) {
 
-			UsersFactory.create($scope.newCompany, function() {
-					            	UsersFactory.getCompanies(function(data) {
-					            		$scope.companies = data;
-					           		 });
-					          	});
-			$scope.newCompany = {};
-    	});	
+	// UsersFactory.create($scope.newCompany, function() {
+	// 		            	UsersFactory.getCompanies(function(data) {
+	// 		            		$scope.companies = data;
+	// 		           		 });
+	// 		          	});
+	// $scope.newCompany = {};
+
+	$scope.createProject = function(){
+		UsersFactory.createCompany($scope.newCompany, function(data) {
+				$scope.company = data;
+			});
+		}
+});	
